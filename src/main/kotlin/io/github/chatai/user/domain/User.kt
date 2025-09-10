6a0058093c1e3f717084e311080fb3d44fd165cc
NaMinhyeok <nmh9097@gmail.com>
2025-09-10 22:22:24 +0900

    feat: 회원가입과 로그인을 서비스에서 구현한다

--- src/main/kotlin/io/github/chatai/user/domain/User.kt
+++ src/main/kotlin/io/github/chatai/user/domain/User.kt
@@ -1,10 +1,18 @@
 package io.github.chatai.user.domain
 
 import io.github.chatai.util.TimeProvider
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 import org.springframework.security.crypto.password.PasswordEncoder
 import java.time.LocalDateTime
 
+@Table(name = "users")
+@Entity
 class User(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long?,
     val email: String,
     val password: String,
